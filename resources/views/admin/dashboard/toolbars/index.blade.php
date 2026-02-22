683e7b5e652d16765b336b818c6012e633402c67
learnudm01y <learnudm01y@gmail.com>
2026-02-23 01:54:48 +0200

    delete not important section103

--- resources/views/admin/dashboard/toolbars/index.blade.php
+++ resources/views/admin/dashboard/toolbars/index.blade.php
@@ -476,7 +476,7 @@
                     </div>
                     <!--end::sidebar menu-->
                     <!--begin::Footer-->
-                    <div class="app-sidebar-footer flex-column-auto pt-2 pb-6 px-6" id="kt_app_sidebar_footer">
+                    {{-- <div class="app-sidebar-footer flex-column-auto pt-2 pb-6 px-6" id="kt_app_sidebar_footer">
                         <a href="https://preview.keenthemes.com/html/metronic/docs"
                             class="btn btn-flex flex-center btn-custom btn-primary overflow-hidden text-nowrap px-0 h-40px w-100"
                             data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-dismiss-="click"
@@ -487,7 +487,7 @@
                                 <span class="path2"></span>
                             </i>
                         </a>
-                    </div>
+                    </div> --}}
                     <!--end::Footer-->
                 </div>
                 <!--end::Sidebar-->
